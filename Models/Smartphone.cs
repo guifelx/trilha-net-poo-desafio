e42741bc904445e84b6cce055e02636d33c34575
Guilherme Felix <guilhermecazagrande@gmail.com>
2024-01-05 00:54:13 -0300

    Desafio POO C# bootcamp DIO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,54 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string modelo;
+        private string imei;
+        private int memoria;
+        public string Modelo
+        {
+            get
+            {
+                return modelo;
+            }
 
-        public Smartphone(string numero)
+            set
+            {
+                modelo = value;
+            }
+        }
+
+        public string Imei
+        {
+            get
+            {
+                return imei;
+            }
+
+            set
+            {
+                imei = value;
+            }
+        }
+
+        public int Memoria
+        {
+            get
+            {
+                return memoria;
+            }
+
+            set
+            {
+                memoria = value;
+            }
+        }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei; 
+            Memoria = memoria; 
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("------ Iphone ------");
+Iphone iphone = new(numero: "+552148599657", modelo: "Iphone 14 pro-max", imei: "1255548", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("------ Nokia ------");
+Nokia nokia = new(numero: "+551125684732", modelo: "Nokia 110 dual chip", imei: "9304821", memoria:128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Uber");
